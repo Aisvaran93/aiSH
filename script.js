f81985b5081cf5e4cdbe6159b61bd3f677602c85
Aisvaran93 <chandramohan.aisvaran@gmail.com>
2025-02-11 18:20:41 +0200

    Added frontend for GitHub Pages

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function sendMessage() {
 
     chatbox.innerHTML += `<p><b>You:</b> ${message}</p>`;
 
-    fetch("http://127.0.0.1:5000/chat", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ message: message })
-    })
+fetch("https://aish-jy9f.onrender.com/chat", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message: message })
+})
     .then(response => response.json())
     .then(data => {
         let reply = data.choices[0].message.content;
